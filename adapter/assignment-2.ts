a17a069f93ade3fcce545a838718f90490bf3097
Lee-Orr <leeorr@gmail.com>
2024-03-15 14:27:55 +0000

    rename package and adjust adapter structure

---
+++ adapter/assignment-2.ts
@@ -0,0 +1,51 @@
+type BookID = string;
+
+interface Book {
+    id?: BookID,
+    name: string,
+    author: string,
+    description: string,
+    price: number,
+    image: string,
+};
+
+async function listBooks(filters?: Array<{from?: number, to?: number}>) : Promise<Book[]>{
+    // We want to generate the query string to match the format expected by qs: https://www.npmjs.com/package/qs
+    let query = filters?.map(({from, to}, index) => {
+        let result = "";
+        if (from) {
+            result += `&filters[${index}][from]=${from}`;
+        }
+        if (to) {
+            result += `&filters[${index}][to]=${to}`
+        }
+        return result;
+    }).join("&") ?? "";
+
+    // We then make the request
+    let result = await fetch(`http://localhost:3000/books?${query}`);
+
+    if (result.ok) {
+        // And if it is valid, we parse the JSON result and return it.
+        return await result.json();
+    } else {
+        throw new Error("Failed to fetch books");
+    }
+}
+
+async function createOrUpdateBook(book: Book): Promise<BookID> {
+    throw new Error("Todo");
+}
+
+async function removeBook(book: BookID): Promise<void> {
+    throw new Error("Todo");    
+}
+
+const assignment = "assignment-2";
+
+export default {
+    assignment,
+    createOrUpdateBook,
+    removeBook,
+    listBooks
+};
